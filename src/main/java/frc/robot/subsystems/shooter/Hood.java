5eeae39ebbf42ecd958c4dc8426ed16152df3b33
ryancave282 <ryancueva282@proton.me>
2026-01-23 21:22:30 -0600

    fix hood

--- src/main/java/frc/robot/subsystems/shooter/Hood.java
+++ src/main/java/frc/robot/subsystems/shooter/Hood.java
@@ -1,49 +1,42 @@
 package frc.robot.subsystems.shooter;
 
-import static edu.wpi.first.units.Units.*;
-
-import com.ctre.phoenix6.hardware.CANcoder;
-import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.math.controller.ArmFeedforward;
-import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import yams.gearing.GearBox;
-import yams.gearing.MechanismGearing;
-import yams.mechanisms.config.ArmConfig;
-import yams.mechanisms.positional.Arm;
-import yams.motorcontrollers.SmartMotorController;
-import yams.motorcontrollers.SmartMotorControllerConfig;
-import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
-import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
-import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
-import yams.motorcontrollers.remote.TalonFXWrapper;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import frc.robot.DroidRageConstants;
+import frc.utility.motor.MotorConstants;
+import frc.utility.motor.MotorConstants.Direction;
+import frc.utility.template.ArmTemplate;
+import frc.utility.template.SubsystemConstants;
+import frc.utility.template.SubsystemConstants.EncoderType;
 
-public class Hood extends SubsystemBase {
-    private final TalonFX hoodMotor = new TalonFX(1);
-    private final SmartMotorControllerConfig motorConfig = new SmartMotorControllerConfig(this)
-        .withClosedLoopController(4, 0, 0, DegreesPerSecond.of(180), DegreesPerSecondPerSecond.of(90))
-        .withFeedforward(new ArmFeedforward(0, 0, 0, 0))
-        .withExternalEncoder(new CANcoder(0))
-        .withExternalEncoderInverted(true)
-        .withExternalEncoderGearing(new MechanismGearing(GearBox.fromReductionStages(3, 4)))
-        .withExternalEncoderZeroOffset(Degrees.of(0))
-        .withUseExternalFeedbackEncoder(true)
-        .withSoftLimit(Degrees.of(-30), Degrees.of(100))
-        .withGearing(new MechanismGearing(GearBox.fromReductionStages(3, 4)))
-        .withIdleMode(MotorMode.BRAKE)
-        .withTelemetry("HoodMotor", TelemetryVerbosity.HIGH)
-        .withStatorCurrentLimit(Amps.of(40))
-        .withMotorInverted(false)
-        .withClosedLoopRampRate(Seconds.of(0.25))
-        .withOpenLoopRampRate(Seconds.of(0.25))
-        .withControlMode(ControlMode.CLOSED_LOOP);
-    private final SmartMotorController smartMotorController = new TalonFXWrapper(hoodMotor,DCMotor.getKrakenX60(1), motorConfig);
-    private final ArmConfig armCfg = new ArmConfig(smartMotorController)
-        .withLength(Feet.of(3))
-        .withMass(Pounds.of(1));
+public class Hood extends ArmTemplate {
+    private static final SubsystemConstants constants = new SubsystemConstants()
+        .withConversionFactor(1)
+        .withEncoderType(EncoderType.ABSOLUTE)
+        .withLowerLimit(0)
+        .withUpperLimit(0)
+        .withName("Hood")
+        .withOffset(0)
+        .withMainNum(0);
 
-    private final Arm hood = new Arm(armCfg);
+    private static final MotorConstants motorConstants = new MotorConstants() 
+        .withCANBus(DroidRageConstants.rioCanBus)
+        .withDirection(Direction.Forward)
+        .withIdleMode(NeutralModeValue.Brake)
+        .withConversionFactor( 1)
+        .withSupplyCurrentLimit(70)
+        .withStatorCurrentLimit(70);
 
-     
+    public Hood(boolean isEnabled) {
+        super(isEnabled, 
+            new ProfiledPIDController(0, 0, 0,
+            new TrapezoidProfile.Constraints(0, 0)), 
+            new ArmFeedforward(0, 0, 0), 
+            constants, 
+            null, 
+            motorConstants);
+    }
 }
