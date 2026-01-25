0d0179c0b2de7139b0ca925c0bbb1f2bfb1bd291
ryancave282 <ryancueva282@proton.me>
2026-01-24 18:22:41 -0600

    begin controlling intake and pivot together

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,39 +1,90 @@
 package frc.robot.subsystems.intake;
 
-import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import lombok.Getter;
 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
-import frc.robot.DroidRageConstants;
-import frc.utility.motor.MotorConstants;
-import frc.utility.motor.MotorConstants.Direction;
-import frc.utility.template.FlywheelTemplate;
-import frc.utility.template.SubsystemConstants;
-import frc.utility.template.SubsystemConstants.EncoderType;
+public class Intake {
+    public enum IntakeValue{
+        STOP(0,0),
 
-public class Intake extends FlywheelTemplate {
-    private static final SubsystemConstants constants = new SubsystemConstants()
-        .withConversionFactor(1)
-        .withEncoderType(EncoderType.INTEGRATED)
-        .withLowerLimit(0)
-        .withUpperLimit(0)
-        .withName("Intake")
-        .withOffset(0)
-        .withMainNum(0);
+        INTAKE(0,0),
+        OUTTAKE(0,0),
 
-    private static final MotorConstants motorConstants = new MotorConstants() 
-        .withCANBus(DroidRageConstants.rioCanBus)
-        .withDirection(Direction.Forward)
-        .withIdleMode(NeutralModeValue.Brake)
-        .withConversionFactor( 1)
-        .withSupplyCurrentLimit(70)
-        .withStatorCurrentLimit(70);
+        HOLD(0,0)
 
-    public Intake(boolean isEnabled) {
-        super(isEnabled,
-            new PIDController(0, 0, 0), 
-            new SimpleMotorFeedforward(0, 0, 0), 
-            constants, 
-            motorConstants);
+        ;
+
+        /*
+        @Getter is an annotation from the lombok plugin.
+        It creates a method to return stuff without creating our own getters.
+        */ 
+        @Getter private final double pivotAngle;
+        @Getter private final double intakeSpeed;
+
+
+        private IntakeValue(double pivotAngle, double intakeSpeed){
+            this.pivotAngle = pivotAngle;
+            this.intakeSpeed = intakeSpeed;
+        }
+        
+        private IntakeValue(IntakeValue value) {
+            this.pivotAngle = value.pivotAngle;
+            this.intakeSpeed = value.intakeSpeed;
+        }
+    }
+
+    @Getter private final Pivot pivot;
+    @Getter private final IntakeWheel intakeWheel;
+
+    private IntakeValue position;
+
+    public Intake(Pivot pivot, IntakeWheel intakeWheel){
+        this.pivot = pivot;
+        this.intakeWheel = intakeWheel;
+        pivot.setTargetPositionDegrees(IntakeValue.STOP.pivotAngle);
+        intakeWheel.setTargetVelocity(IntakeValue.STOP.intakeSpeed);
+        position = IntakeValue.STOP;
+        SmartDashboard.putString("Carriage/Position", "INTAKE_HPS");
+    }
+    
+    public IntakeValue getPosition() {
+        return position;
+    }
+    
+    public Command setPositionCommand(IntakeValue targetPos) {
+        return Commands.sequence(    
+            switch (targetPos) {      
+                default -> 
+                    new SequentialCommandGroup(
+                        pivot.setTargetPositionCommand(targetPos.getPivotAngle()),
+                        intakeWheel.setTargetVelocityCommand(targetPos.intakeSpeed)
+                    );
+            },
+            
+            new InstantCommand(() -> position=targetPos),
+            new InstantCommand(()-> SmartDashboard.putString("Intake/Position", targetPos.name()))
+        );
+    }
+    /** Not something to Command anything other than to make the writers reflect the position */
+     public Command setPosition(IntakeValue targetPos) {
+         return new SequentialCommandGroup(
+            new InstantCommand(()-> position = targetPos)
+        );
+    }
+
+    public boolean isIntakeValue(IntakeValue value){
+        return position == value;
+    }
+
+    public void isPushed(){  //Might need to put a timeout period for this AND might need a switch to turn it off when mechanism is off
+        boolean isPushed = pivot.getSetpointError()> 20 && pivot.getMotor().getVoltage()>5;
+        if (isPushed){
+            setPositionCommand(IntakeValue.HOLD);
+        }
     }
 }
---
+++ src/main/java/frc/robot/subsystems/intake/IntakeWheel.java
@@ -0,0 +1,39 @@
+package frc.robot.subsystems.intake;
+
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import frc.robot.DroidRageConstants;
+import frc.utility.motor.MotorConstants;
+import frc.utility.motor.MotorConstants.Direction;
+import frc.utility.template.FlywheelTemplate;
+import frc.utility.template.SubsystemConstants;
+import frc.utility.template.SubsystemConstants.EncoderType;
+
+public class IntakeWheel extends FlywheelTemplate {
+    private static final SubsystemConstants constants = new SubsystemConstants()
+        .withConversionFactor(1)
+        .withEncoderType(EncoderType.INTEGRATED)
+        .withLowerLimit(0)
+        .withUpperLimit(0)
+        .withName("Intake")
+        .withOffset(0)
+        .withMainNum(0);
+
+    private static final MotorConstants motorConstants = new MotorConstants() 
+        .withCANBus(DroidRageConstants.rioCanBus)
+        .withDirection(Direction.Forward)
+        .withIdleMode(NeutralModeValue.Brake)
+        .withConversionFactor( 1)
+        .withSupplyCurrentLimit(70)
+        .withStatorCurrentLimit(70);
+
+    public IntakeWheel(boolean isEnabled) {
+        super(isEnabled,
+            new PIDController(0, 0, 0), 
+            new SimpleMotorFeedforward(0, 0, 0), 
+            constants, 
+            motorConstants);
+    }
+}
