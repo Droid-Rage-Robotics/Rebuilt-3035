321b12068e606e193022479a3e7eb54cf0c88aea
ryancave282 <ryancueva282@proton.me>
2026-01-24 18:24:23 -0600

    work on candle

--- src/main/java/frc/utility/CANdleEx.java
+++ src/main/java/frc/utility/CANdleEx.java
@@ -3,6 +3,17 @@ package frc.utility;
 import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.configs.CANdleConfiguration;
 import com.ctre.phoenix6.hardware.CANdle;
+import com.ctre.phoenix6.signals.AnimationDirectionValue;
+import com.ctre.phoenix6.signals.Enable5VRailValue;
+import com.ctre.phoenix6.signals.LossOfSignalBehaviorValue;
+import com.ctre.phoenix6.signals.RGBWColor;
+import com.ctre.phoenix6.signals.StatusLedWhenActiveValue;
+import com.ctre.phoenix6.signals.StripTypeValue;
+
+import edu.wpi.first.wpilibj.util.Color;
+
+import com.ctre.phoenix6.controls.RainbowAnimation;
+import com.ctre.phoenix6.controls.SingleFadeAnimation;
 
 import frc.robot.DroidRageConstants;
 
@@ -12,9 +23,22 @@ public class CANdleEx {
     private final int deviceId;
 
     private CANdleEx(int deviceId, CANBus canBus) {
-        this.deviceId=deviceId;
+        this.deviceId = deviceId;
         this.candle = new CANdle(deviceId, canBus);
         this.config = new CANdleConfiguration();
+        config.statusLedOffWhenActive = true;
+        config.disableWhenLOS = false;
+        config.stripType = LEDStripType.GRB;
+        config.brightnessScalar = 0.1;
+        config.vBatOutputMode = VBatOutputMode.Modulated;
+
+        config.CANdleFeatures.Enable5VRail = Enable5VRailValue.Enabled;
+        config.CANdleFeatures.StatusLedWhenActive = StatusLedWhenActiveValue.Enabled;
+        config.LED.LossOfSignalBehavior = LossOfSignalBehaviorValue.DisableLEDs;
+        config.LED.StripType = StripTypeValue.RGB;
+        config.LED.BrightnessScalar = 0.9;
+        candle.getConfigurator().apply(config);
+
     }
 
     public static CANdleEx create(int deviceId, CANBus canBus) {
@@ -23,5 +47,14 @@ public class CANdleEx {
 
     public static CANdleEx create(int deviceId) {
         return new CANdleEx(deviceId, DroidRageConstants.rioCanBus);
+    }
+
+    public void efsd() {
+        candle.setControl(
+                new RainbowAnimation(8, 10)
+                        .withDirection(AnimationDirectionValue.Forward));
+        new SingleFadeAnimation(8, 16)
+                .withColor(new RGBWColor(Color.kAntiqueWhite));
+
     }
 }
