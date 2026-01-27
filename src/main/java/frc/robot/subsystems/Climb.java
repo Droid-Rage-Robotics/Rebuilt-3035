4badc8c38dd3f299269c77a3412bb32e4cd6f477
ryancave282 <ryancueva282@proton.me>
2026-01-26 22:08:17 -0600

    create the climb

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -1,16 +1,58 @@
-// package frc.robot.subsystems;
+package frc.robot.subsystems;
 
-// import edu.wpi.first.math.controller.ArmFeedforward;
-// import edu.wpi.first.math.controller.PIDController;
-// import edu.wpi.first.math.trajectory.TrapezoidProfile;
-// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-// import frc.robot.DroidRageConstants.Control;
-// import frc.utility.motor.MotorBase;
-// import frc.utility.motor.TalonEx;
-// import frc.utility.motor.MotorBase.Direction;
-// import frc.utility.motor.MotorBase.ZeroPowerMode;
-// import frc.utility.template.ArmTemplate;
+import static edu.wpi.first.units.Units.Inches;
 
-// public class Climb extends ArmTemplate {
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.units.measure.Distance;
+import frc.robot.DroidRageConstants;
+import frc.utility.motor.MotorConstants.Direction;
+import frc.utility.motor.MotorConstants;
+import frc.utility.template.ElevatorTemplate;
+import frc.utility.template.SubsystemConstants;
+import frc.utility.template.SubsystemConstants.EncoderType;
+import lombok.Getter;
+
+public class Climb extends ElevatorTemplate {
+    public enum ClimbValue {
+        START(Inches.of(0)),
+        CLIMB(Inches.of(0));
+
+        @Getter private final Distance height;
+
+        private ClimbValue(Distance height) {
+            this.height = height;
+        }
+
+    }
+
+    private static final SubsystemConstants constants = new SubsystemConstants()
+        .withConversionFactor(1)
+        .withEncoderType(EncoderType.INTEGRATED)
+        .withLowerLimit(0)
+        .withUpperLimit(0)
+        .withName("Turret")
+        .withOffset(0)
+        .withMainNum(0);
     
-// }
+    private static final MotorConstants motorConstants = new MotorConstants() 
+        .withCANBus(DroidRageConstants.rioCanBus)
+        .withDirection(Direction.Forward)
+        .withIdleMode(NeutralModeValue.Brake)
+        .withConversionFactor( 1)
+        .withSupplyCurrentLimit(70)
+        .withStatorCurrentLimit(70);
+
+    public Climb(boolean isEnabled) {
+        super(isEnabled, 
+            new ProfiledPIDController(0, 0, 0, 
+            new TrapezoidProfile.Constraints(0, 0)), 
+            new ElevatorFeedforward(0, 0, 0), 
+            constants, 
+            null, 
+            motorConstants);
+    }
+}
