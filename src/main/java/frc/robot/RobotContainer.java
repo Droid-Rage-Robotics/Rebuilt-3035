11f3093a6519b63237eb32d519b346e5df2f3aae
S0phi333 <sophie.l.galvan@gmail.com>
2026-02-13 13:46:45 -0600

    Teleop buttons (STILL being worked on just set up for the mean while)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -21,13 +21,17 @@ import frc.robot.commands.Turn180Degrees;
 // import frc.robot.commands.SysId.SysIdRoutineCommand;
 // import frc.robot.commands.drive.TeleopAlign;
 import frc.robot.commands.manual.SwerveDriveTeleop;
-import frc.robot.commands.manual.Turning;
+import frc.robot.subsystems.Climb;
 import frc.robot.subsystems.Indexer;
+import frc.robot.subsystems.Indexer.IndexerValue;
 import frc.robot.subsystems.Light;
 import frc.robot.subsystems.drive.SwerveDrive;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.intake.Intake.IntakeValue;
+import frc.robot.subsystems.shooter.Kicker;
+import frc.robot.subsystems.shooter.Kicker.KickerValue;
 import frc.robot.subsystems.shooter.Shooter;
+import frc.robot.subsystems.shooter.Shooter.ShooterMode;
 import frc.robot.subsystems.vision.Vision;
 
 public class RobotContainer {
@@ -43,7 +47,9 @@ public class RobotContainer {
 	public void configureTeleOpBindings(
 		SwerveDrive drive,
 		Intake intake,
+		Climb climb,
 		Indexer indexer,
+		Kicker kicker,
 		Shooter shooter,
 		Vision vision,
 		Light light
@@ -52,6 +58,8 @@ public class RobotContainer {
 			drive.setDefaultCommand(new SwerveDriveTeleop(drive, driver));
 			// drive.setDefaultCommand(new Turning(drive, driver));
 			// climb.setDefaultCommand(new ClimbTeleop(climb, operator::getRightY));
+			// climb.setDefaultCommand(new ManualClimb(climb, operator::getLeftY));
+
 			// light.setDefaultCommand(new LightCommand(light));
 			driver.rightTrigger()
 				.onTrue(intake.setPositionCommand(IntakeValue.INTAKE))
@@ -59,11 +67,28 @@ public class RobotContainer {
 			driver.leftTrigger()
 				.onTrue(intake.setPositionCommand(IntakeValue.OUTTAKE))
 				.onFalse(intake.setPositionCommand(IntakeValue.STOP));
-			
-			
-			
-				operator.y()
-				.onTrue(new InstantCommand(null, null));
+
+			// operator.rightTrigger()
+    		// 	.onTrue(climb.setClimbPositionCommand(ClimbValue.CLIMB))
+    		// 	.onFalse(climb.setClimbPositionCommand(ClimbValue.START));
+    			
+			// operator.rightBumper()
+    		// 	.onTrue(indexer.setIndexerCommand(IndexerValue.INTAKE))
+   			// 	.onFalse(indexer.setIndexerCommand(IndexerValue.STOP));
+
+			// operator.leftBumper()
+			// 	.onTrue(kicker.setKickerCommand(KickerValue.INTAKE))
+			// 	.onFalse(indexer.setKickerCommand(IndexerValue.STOP));
+
+			operator.a()
+    			.onTrue(shooter.setShooterModeCommand(ShooterMode.HOLD)); //LED strip: indicate the mode, one automatic (automates itself), three positions
+			operator.b()
+    			.onTrue(shooter.setShooterModeCommand(ShooterMode.OPPOSITE));
+			operator.x()
+				.onTrue(shooter.setShooterModeCommand(ShooterMode.SCORE));
+			operator.y()
+				.onTrue(shooter.setShooterModeCommand(ShooterMode.HOARD));
+				
 	}
 
 	public void testDrive(CommandXboxController driver, SwerveDrive drive, Vision vision) {
