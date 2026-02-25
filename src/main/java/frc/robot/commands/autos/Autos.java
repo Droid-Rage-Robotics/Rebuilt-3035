e395659a3d636e0db9f67537840f622c2b349853
S0phi333 <sophie.l.galvan@gmail.com>
2026-02-25 15:38:07 -0600

    auto changes

--- src/main/java/frc/robot/commands/autos/Autos.java
+++ src/main/java/frc/robot/commands/autos/Autos.java
@@ -2,9 +2,8 @@ package frc.robot.commands.autos;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.subsystems.drive.SwerveDrive;
 import frc.robot.subsystems.Indexer;
+import frc.robot.subsystems.drive.SwerveDrive;
 import frc.robot.subsystems.shooter.Kicker;
 import frc.robot.subsystems.shooter.Shooter;
 import frc.robot.subsystems.vision.Vision;
@@ -19,7 +18,59 @@ public final class Autos {
                 .build()
         );
     }
-    public static Command crossNeuralOutpost(SwerveDrive drive, Shooter shooter, Indexer indexer, Kicker kicker, Vision vision) {//Top Red/Bottom Blue
+    public static Command NeutralToTrenchDepot(SwerveDrive drive, Shooter shooter, Indexer indexer, Kicker kicker, Vision vision) {//Top Red/Bottom Blue
+        return new SequentialCommandGroup(
+            PathPlannerFollow.create(drive, "NeutralToTrenchDepot")
+                .setMaxVelocity(6)
+                .setAcceleration(6)
+                .build()
+        );
+    }
+    public static Command TrenchToNeutralDepot(SwerveDrive drive, Shooter shooter, Indexer indexer, Kicker kicker, Vision vision) {//Top Red/Bottom Blue
+        return new SequentialCommandGroup(
+            PathPlannerFollow.create(drive, "TrenchToNeutralDepot")
+                .setMaxVelocity(6)
+                .setAcceleration(6)
+                .build()
+        );
+    }
+    
+    public static Command TrenchToTowerDepot(SwerveDrive drive, Shooter shooter, Indexer indexer, Kicker kicker, Vision vision) {//Top Red/Bottom Blue
+        return new SequentialCommandGroup(
+            PathPlannerFollow.create(drive, "TrenchToTowerDepot")
+                .setMaxVelocity(6)
+                .setAcceleration(6)
+                .build()
+        );
+    }
+    public static Command DepotToTower(SwerveDrive drive, Shooter shooter, Indexer indexer, Kicker kicker, Vision vision) {//Top Red/Bottom Blue
+        return new SequentialCommandGroup(
+            // new WaitCommand(4),
+            PathPlannerFollow.create(drive, "DepotToTower")
+                .setMaxVelocity(6)
+                .setAcceleration(6)
+                .build()
+        );
+    }
+    public static Command HPSToDepot(SwerveDrive drive, Shooter shooter, Indexer indexer, Kicker kicker, Vision vision) {//Top Red/Bottom Blue
+        return new SequentialCommandGroup(
+            PathPlannerFollow.create(drive, "HPSToDepot")
+                .setMaxVelocity(6)
+                .setAcceleration(6)
+                .build()
+           
+        );
+    }
+    public static Command hubToHPS(SwerveDrive drive, Shooter shooter, Indexer indexer, Kicker kicker, Vision vision) {//Top Red/Bottom Blue
+        return new SequentialCommandGroup(
+            PathPlannerFollow.create(drive, "hubToHPS")
+                .setMaxVelocity(6)
+                .setAcceleration(6)
+                .build()
+        );
+    }
+    
+    public static Command crossNeutralOutpost(SwerveDrive drive, Shooter shooter, Indexer indexer, Kicker kicker,  Vision vision) {//Top Red/Bottom Blue
         return new SequentialCommandGroup(
             PathPlannerFollow.create(drive, "crossNeutralOutpost")
                 .setMaxVelocity(6)
@@ -27,75 +78,31 @@ public final class Autos {
                 .build()
         );
     }
-    public static Command middle(SwerveDrive drive, Shooter shooter, Indexer indexer, Kicker kicker, Vision vision) {//Top Red/Bottom Blue
+    public static Command NeutralToTrenchOutpost(SwerveDrive drive, Shooter shooter, Indexer indexer, Kicker kicker, Vision vision) {//Top Red/Bottom Blue
         return new SequentialCommandGroup(
-            PathPlannerFollow.create(drive, "middle")
+            PathPlannerFollow.create(drive, "NeutralToTrenchOutpost")
                 .setMaxVelocity(6)
                 .setAcceleration(6)
                 .build()
         );
     }
-    
-    public static Command neutralDepot(SwerveDrive drive, Shooter shooter, Indexer indexer, Kicker kicker, Vision vision) {//Top Red/Bottom Blue
+    public static Command TrenchToNeutralOutpost(SwerveDrive drive, Shooter shooter, Indexer indexer, Kicker kicker, Vision vision) {//Top Red/Bottom Blue
         return new SequentialCommandGroup(
-            PathPlannerFollow.create(drive, "neutralDepot")
+            PathPlannerFollow.create(drive, "TrenchToNeutralOutpost")
                 .setMaxVelocity(6)
                 .setAcceleration(6)
                 .build()
         );
     }
-    public static Command neutralOutpost(SwerveDrive drive, Shooter shooter, Indexer indexer, Kicker kicker, Vision vision) {//Top Red/Bottom Blue
-        return new SequentialCommandGroup(
-            // new WaitCommand(4),
-            PathPlannerFollow.create(drive, "neutralOutpost")
-                .setMaxVelocity(6)
-                .setAcceleration(6)
-                .build()
-        );
-    }
-    public static Command autoName(SwerveDrive drive, Shooter shooter, Indexer indexer, Kicker kicker, Vision vision) {//Top Red/Bottom Blue
-        return new SequentialCommandGroup(
-            PathPlannerFollow.create(drive, "auto name")
-                .setMaxVelocity(6)
-                .setAcceleration(6)
-                .build()
-           
-        );
-    }
-    // public static Command autoName(SwerveDrive drive, Shooter shooter, Indexer indexer, Kicker kicker, Vision vision) {//Top Red/Bottom Blue
-    //     return new SequentialCommandGroup(
-    //         PathPlannerFollow.create(drive, "auto name")
-    //             .setMaxVelocity(6)
-    //             .setAcceleration(6)
-    //             .build()
-    //     );
-    // }
-    
-    // public static Command autoName(SwerveDrive drive, Shooter shooter, Indexer indexer, Kicker kicker,  Vision vision) {//Top Red/Bottom Blue
-    //     return new SequentialCommandGroup(
-    //         PathPlannerFollow.create(drive, "auto name")
-    //             .setMaxVelocity(6)
-    //             .setAcceleration(6)
-    //             .build()
-    //     );
-    // }
-    // public static Command autoName(SwerveDrive drive, Shooter shooter, Indexer indexer, Kicker kicker, Vision vision) {//Top Red/Bottom Blue
-    //     return new SequentialCommandGroup(
-    //         PathPlannerFollow.create(drive, "auto name")
-    //             .setMaxVelocity(6)
-    //             .setAcceleration(6)
-    //             .build()
-    //     );
-    // }
-    // public static Command autoName(SwerveDrive drive, Shooter shooter, Indexer indexer, Kicker kicker, Vision vision) {//Top Red/Bottom Blue
-    //     return new SequentialCommandGroup(
-    //         PathPlannerFollow.create(drive, "auto name")
-    //             .setMaxVelocity(6)
-    //             .setAcceleration(6)
-    //             .build()
-    //     );
-    // }
    
+    public static Command TrenchToTowerOutpost(SwerveDrive drive, Shooter shooter, Indexer indexer, Kicker kicker, Vision vision) {//Top Red/Bottom Blue
+        return new SequentialCommandGroup(
+            PathPlannerFollow.create(drive, "TrenchToTowerOutpost")
+                .setMaxVelocity(6)
+                .setAcceleration(6)
+                .build()
+        );
+    }
     // public static Command autoName(SwerveDrive drive, Shooter shooter, Indexer indexer, Kicker kicker, Vision vision) {//Top Red/Bottom Blue
     //     return new SequentialCommandGroup(
     //         PathPlannerFollow.create(drive, "auto name")
