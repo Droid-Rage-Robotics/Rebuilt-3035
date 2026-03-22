434ef5b2cdbfeea1280955c19593a3f3da4d44c9
Sai Varshini Chinnasani <saivarshini.chinnasani@gmail.com>
2026-03-21 22:11:24 -0500

    Updated Autos

--- src/main/java/frc/robot/commands/shooter/DRShooter.java
+++ src/main/java/frc/robot/commands/shooter/DRShooter.java
@@ -78,7 +78,7 @@ public class DRShooter extends Command{
 
     @Override 
     public void execute(){
-        //Get the predicted robot pose based on current velocity to improve targeting while moving for 1 Second ahead
+        // Get the predicted robot pose based on current velocity to improve targeting while moving for 1 Second ahead
         // Pose2d lookAheadPose = predictPosePos(
         //     drive.getState().Pose, 
         //     drive.getCurrentRobotChassisSpeeds());
