ad61b417018b1f44d3ddd8833ab373410cef395a
Sophie Galvan <sophie.l.galvan@gmail.com>
2026-04-04 13:47:02 -0500

    After Playoff 1

--- src/main/java/frc/robot/subsystems/intake/Pivot.java
+++ src/main/java/frc/robot/subsystems/intake/Pivot.java
@@ -33,8 +33,8 @@ public class Pivot extends ArmTemplate {
         .withConversionFactor(1)
         .withDirection(Direction.Forward)
         .withIdleMode(NeutralModeValue.Brake)
-        .withStatorCurrentLimit(35) //Reefscape 50
-        .withSupplyCurrentLimit(25); //Reefscape None
+        .withStatorCurrentLimit(45) //Reefscape 50
+        .withSupplyCurrentLimit(35); //Reefscape None
         //TODO: Uncomment and check the command to turn on and off current limit
 
 
