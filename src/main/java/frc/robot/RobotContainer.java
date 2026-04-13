511cdeaa304e2b6bb9aa1f5e093034119f846dfc
ryancave282 <ryancueva282@gmail.com>
2026-04-12 19:44:45 -0500

    swoop auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -32,22 +32,22 @@ import frc.utility.DRAreaManager;
 
 public class RobotContainer {
 	private final SwerveConfig swerveConfig = new SwerveConfig();
-	public final SwerveDrive drive = new SwerveDrive(true, swerveConfig);
+	public final SwerveDrive drive = new SwerveDrive(false, swerveConfig);
 	private final Vision vision = new Vision();
 	private final Intake intake = new Intake(
-        new Pivot(true),
-        new IntakeWheel(true)
+        new Pivot(false),
+        new IntakeWheel(false)
     );
     // private final Indexer indexer = new Indexer(true);
 	private final Indexer indexer = new Indexer(
-		new BottomRollers(true), 
-		new TopRoller(true)
+		new BottomRollers(false), 
+		new TopRoller(false)
 	);
 
-    private final Kicker kicker = new Kicker(true);
+    private final Kicker kicker = new Kicker(false);
     private final Shooter shooter = new Shooter(
-        new Turret(true),
-        new Hood(true),
+        new Turret(false),
+        new Hood(false),
         new ShooterWheel(true)
     );
 
@@ -72,6 +72,7 @@ public class RobotContainer {
 		configureTeleOpBindings();
 		// testShootingMove();
 		// driver.a().onTrue(shooter.getTurret().getSysIdCommand());
+		// driver.a().onTrue(shooter.getShooterWheel().getSysIdCommand());
 		// testCurrentChangingLimits();
 	}
 
