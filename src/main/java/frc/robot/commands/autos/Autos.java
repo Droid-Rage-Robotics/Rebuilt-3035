8a30eef6b373bc0d03ccb10102522849a5d1a1e9
Sai Varshini Chinnasani <saivarshini.chinnasani@gmail.com>
2026-04-23 17:02:48 -0500

    Testing 4

--- src/main/java/frc/robot/commands/autos/Autos.java
+++ src/main/java/frc/robot/commands/autos/Autos.java
@@ -355,30 +355,31 @@ public final class Autos {
     }
 
     public static Command testPath(SwerveDrive drive, Intake intake, Indexer indexer, Kicker kicker, Shooter shooter, Vision vision) {
-        // return new SequentialCommandGroup(
+        return new SequentialCommandGroup(
+            new ParallelCommandGroup (
+                PathPlannerPathFollow.create(drive, "ForwardTest", true)
+                    .withMaxVelocity(11)
+                    .withMaxAcceleration(11)
+                    .build(),
+                new AutoDRShooter(drive, shooter)
+            )
+        );
+        // return new SequentialCommandGroup (
         //     new ParallelCommandGroup (
         //         PathPlannerPathFollow.create(drive, "TrenchToNeutralRightS", true)
-        //             .withMaxVelocity(11)
-        //             .withMaxAcceleration(11)
-        //             .build(),
-        //         new AutoDRShooter(drive, shooter)
-        //     )
+        //             .withMaxVelocity(6)
+        //             .withMaxAcceleration(6)
+        //             .build()
+        //     ),
+        //     PathPlannerPathFollow.create(drive, "NeutralToTrenchRightS")
+        //         .withMaxVelocity(6)
+        //         .withMaxAcceleration(6)
+        //         .build(),
+        //     new WaitCommand(3),
+        //     new AutoDRShooter(drive, shooter),
+        //     new WaitCommand(19),
+        //     AutoCommands.resetBot(shooter, indexer, kicker, intake)
         // );
-        return new SequentialCommandGroup (
-            new ParallelCommandGroup (
-                PathPlannerPathFollow.create(drive, "TrenchToNeutralRightS", true)
-                    .withMaxVelocity(6)
-                    .withMaxAcceleration(6)
-                    .build()
-            ),
-            PathPlannerPathFollow.create(drive, "NeutralToTrenchRightS")
-                .withMaxVelocity(6)
-                .withMaxAcceleration(6)
-                .build(),
-            new AutoDRShooter(drive, shooter),
-            new WaitCommand(19),
-            AutoCommands.resetBot(shooter, indexer, kicker, intake)
-        );
     }
 
     // public static Command newRightOutpost(String depth, SwerveDrive drive, Intake intake, Indexer indexer, Kicker kicker, Shooter shooter, Vision vision) {
