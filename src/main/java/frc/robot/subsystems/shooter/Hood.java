b6238ee666d24a9fad6ef74668667a203adc369f
Sophie Galvan <sophie.l.galvan@gmail.com>
2026-04-29 18:34:40 -0500

    no let hood come up in trench

--- src/main/java/frc/robot/subsystems/shooter/Hood.java
+++ src/main/java/frc/robot/subsystems/shooter/Hood.java
@@ -3,7 +3,12 @@ package frc.robot.subsystems.shooter;
 import static edu.wpi.first.units.Units.*;
 
 import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.DroidRageConstants;
+import frc.utility.DRAreaManager;
+import frc.utility.DRAreaManager.Zone;
 import frc.utility.devices.motor.MotorConstants;
 import frc.utility.devices.motor.MotorConstants.Direction;
 import frc.utility.template.ArmTemplate;
@@ -33,8 +38,12 @@ public class Hood extends ArmTemplate {
         .withSupplyCurrentLimit(45)
         .withStatorCurrentLimit(50);
 
+    private final Trigger hoodProtect = new Trigger(()-> (DRAreaManager.getCurrentZone()==Zone.BETWEEN));
+    
     public Hood(boolean isEnabled) {
         super(isEnabled, constants, null, motorConstants);
+
+        hoodProtect.whileTrue(setTargetPositionCommand(Degrees.zero()));
     }
 
     @Override
@@ -44,5 +53,7 @@ public class Hood extends ArmTemplate {
         if (getCurrentAngle().in(Degrees)<0) {
             resetEncoder(Rotation.zero());
         }
+        
     }
+
 }
